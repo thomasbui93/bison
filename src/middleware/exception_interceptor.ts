ea691832505406735e1a5c9251e2789373ebb737
Khoa Bui <thomasbui93@users.noreply.github.com>
2020-10-11 18:22:32 +0100

    Replace normal log with pino logger (#9)

--- src/middleware/exception_interceptor.ts
+++ src/middleware/exception_interceptor.ts
@@ -1,4 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
+import logger from '../helpers/logger'
+const log = logger.child({
+  name: 'exceptionInterceptor'
+})
 
 const getStatus = (err: Error): number => {
   switch (err.name) {
@@ -22,12 +26,12 @@ export default function exceptionInterceptor(err: Error, req: Request, res: Resp
     return next(err)
   }
 
-  console.error('Uncaught exception', {
+  log.error({
     request: req.url,
     method: req.method,
     body: req.body,
     stack: err.stack,
-  })
+  }, 'Uncaught exception.')
 
   return res.status(getStatus(err)).send({
     error: true,

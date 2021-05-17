3ee1804a005d0139b3d77d12f5e272e96e4ef804
Khoa Bui <kbui@zendesk.com>
2021-05-17 23:12:01 +0100

    add audit log based on failure code

--- src/features/audit/Audit.ts
+++ src/features/audit/Audit.ts
@@ -3,21 +3,25 @@ import getSequelize from '../../bootstrap/sequelize'
 
 export const NORMAL_USER = 'NORMAL_USER'
 export const SYSTEM_USER = 'SYSTEM_USER'
+export const ADMIN_USER = 'ADMIN_USER'
 
-export const USER_TYPES = {
+export const USER_TYPES = [
   NORMAL_USER,
-  SYSTEM_USER
-}
+  SYSTEM_USER,
+  ADMIN_USER
+]
 
 export const LOGGED_IN = 'LOGGED_IN'
 export const LOGGED_OUT = 'LOGGED_OUT'
 export const PASSWORD_CHANGED = 'PASSWORD_CHANGED'
+export const TOKEN_VERIFIED = 'TOKEN_VERIFIED'
 
-export const AUTHENTICATION_ACTION = {
+export const AUTHENTICATION_ACTION = [
   LOGGED_IN,
   LOGGED_OUT,
+  TOKEN_VERIFIED,
   PASSWORD_CHANGED
-}
+]
 
 const Audit = getSequelize().define('Audit', {
   user_ref: {
@@ -25,11 +29,11 @@ const Audit = getSequelize().define('Audit', {
     allowNull: false
   },
   user_type: {
-    type: DataTypes.ENUM(NORMAL_USER, SYSTEM_USER),
+    type: DataTypes.ENUM(...USER_TYPES),
     allowNull: false,
   },
   action: {
-    type: DataTypes.ENUM(LOGGED_IN, LOGGED_OUT, PASSWORD_CHANGED),
+    type: DataTypes.ENUM(...AUTHENTICATION_ACTION),
     allowNull: false
   },
   ip: {
--- src/middleware/exception_interceptor.ts
+++ src/middleware/exception_interceptor.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import logger from '../helpers/logger'
+import createAudit from '../features/audit/create_audit'
+import { SYSTEM_USER, NORMAL_USER, LOGGED_IN, TOKEN_VERIFIED, ADMIN_USER, PASSWORD_CHANGED } from '../features/audit/Audit'
+
 const log = logger.child({
   name: 'exceptionInterceptor'
 })
@@ -22,9 +25,36 @@ const getStatus = (err: Error): number => {
   }
 }
 
-export default function exceptionInterceptor(err: Error, req: Request, res: Response, next: NextFunction) {
+const createAuditLog = (err: Error, req: Request) => {
+  switch (err.name) {
+    case 'SystemUserAuthenticationFailed':
+      return createAudit(req.body.name, SYSTEM_USER, req.ip, LOGGED_IN, false);
+    case 'NormalUserAuthenticationFailed':
+      return createAudit(req.body.email, NORMAL_USER, req.ip, LOGGED_IN, false);
+    case 'TokenAuthenticationException':
+      return createAudit(req.body.token, NORMAL_USER, req.ip, TOKEN_VERIFIED, false);
+    case 'AdminAuthenticationFailure':
+      return createAudit(ADMIN_USER, ADMIN_USER, req.ip, LOGGED_IN, false);
+    case 'ChangePasswordFailure':
+      return createAudit(req.body.email, NORMAL_USER, req.ip, PASSWORD_CHANGED, false);
+    default:
+      return null
+  }
+}
+
+export default async function exceptionInterceptor(err: Error, req: Request, res: Response, next: NextFunction) {
   if (res.headersSent) {
     return next(err)
+  }
+  try {
+    await createAuditLog(err, req)
+  } catch(err) {
+    log.error({
+      request: req.url,
+      method: req.method,
+      body: req.body,
+      stack: err.stack,
+    }, 'Failed to create audit log when exception is thrown.')
   }
 
   log.error({
